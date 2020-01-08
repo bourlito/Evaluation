06a7904ce89bd9eb5325e03d51b849743920fce3
etienne <etienne.le-bourlout@hei.yncrea.fr>
2020-01-08 15:43:07 +0100

    param

--- src/code/utils/Parametres.java
+++ src/code/utils/Parametres.java
@@ -2,7 +2,8 @@ package code.utils;
 
 public class Parametres {
 
-    public static final String DOSSIER = "D:\\CPE Solutions\\Evaluation\\";
+    // public static final String DOSSIER = "D:\\CPE Solutions\\Evaluation\\";
+    public static final String DOSSIER = "C:\\Users\\loann\\OneDrive\\Bureau\\Interne\\Èvaluation Collaborateur";
     public static final String FILENAME = "Recap evaluations.xlsx";
 
     public static String DEST;
